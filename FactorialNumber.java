e406b5ffc24da880dbed025a5c5a9fd593e6c120
JJLi514 <lijiaju19900514@gmail.com>
2022-06-27 10:07:09 -0400

    Second version with recursive.

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -1,4 +1,23 @@
 public class FactorialNumber {
+    static boolean otherWays(int n){
+        if (n == 0){
+            return true;
+        }
+        return usingRecursive(n,2);
+    }
+    static boolean usingRecursive(int n, int i){
+        if (n == 1){
+            return true;
+        }
+        if (n % i != 0){
+            return false;
+        }
+        if (n % i == 0 && n >= i){
+            return usingRecursive(n / i,i + 1);
+        }
+        return true;
+    }
+
     static boolean isFactorial(int n){
         for (int i = 1; ; i++){
             if (n % i == 0){
@@ -11,8 +30,8 @@ public class FactorialNumber {
     }
 
     public static void main(String[] args) {
-        int n = 3;
-        boolean ans = isFactorial(n);
-        System.out.println(ans);
+        int n = 24;
+        System.out.println("using Recursive Ans:"+otherWays(n));
+        System.out.println("using loop Ans:"+isFactorial(n));
     }
 }
